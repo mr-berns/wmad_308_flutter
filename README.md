9ceda0d192820603a043bdb9003b2ec6978d32d9
colempep_bernie <zaily.ernie@gmail.com>
2024-02-23 17:54:32 +0800

    flutter test drive

---
+++ README.md
@@ -0,0 +1,2 @@
+<h1>WMAD-308 Multiplatform Mobile Development</h1>
+<h2>Flutter</h2>
---
+++ flutter_application_test_drive_legaspi_lambert/android/app/src/main/kotlin/com/example/flutter_application_test_drive_legaspi_lambert/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_application_test_drive_legaspi_lambert
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
